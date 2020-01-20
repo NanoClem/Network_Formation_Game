2e3053b0fbfcaed8c1af816a8fd5c14a8a49e50c
NanoClem <clemdecoop.pro@gmail.com>
2020-01-20 23:46:15 +0100

    Movings abstracts products

--- games/products/abstracts_products/__init__.py
+++
@@ -1,4 +0,0 @@
-from .NodeEvaluationSystem import NodeEvaluationSystem
-from .ScoreComputationSystem import ScoreComputationSystem
-from .RankingSystem import RankingSystem
-from .TurnSystem import TurnSystem
